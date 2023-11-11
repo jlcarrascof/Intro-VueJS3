050dbe33a10097dceb31c46dc764cf151e55cd09
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-11 14:01:41 -0400

    Start exercise about conditional rendering

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://www.google.com',
+            image: './assets/images/socks_blue.jpg'
         }
-    }    
+    }
 })
